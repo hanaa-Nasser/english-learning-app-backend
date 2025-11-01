bb654c6e07462e387ef1a710ba7be7862134a497
hanaa131 <106615966+hanaa131@users.noreply.github.com>
2025-11-01 14:15:28 +0200

    update ....

--- apps/assignments/views.py
+++ apps/assignments/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from .models import Assignment, AssignmentSubmission
 from .serializers import AssignmentSerializer, AssignmentSubmissionSerializer
@@ -8,11 +10,27 @@ class AssignmentViewSet(viewsets.ModelViewSet):
     serializer_class = AssignmentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    @action(detail=True, methods=['get'], url_path='has-submitted')
+    def has_submitted(self, request, pk=None):
+        user = request.user
+        if not hasattr(user, 'student'):
+            raise PermissionDenied("Only students can check submission status.")
+
+        assignment = self.get_object()
+        submitted = AssignmentSubmission.objects.filter(
+            assignment=assignment,
+            student=user.student
+        ).exists()
+
+        return Response({'has_submitted': submitted})
+
+
 class AssignmentSubmissionViewSet(viewsets.ModelViewSet):
     queryset = AssignmentSubmission.objects.all()
     serializer_class = AssignmentSubmissionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    
     def get_queryset(self):
         user = self.request.user
         if user.role == 'teacher':
@@ -38,6 +56,8 @@ class AssignmentSubmissionViewSet(viewsets.ModelViewSet):
         raise PermissionDenied("You have already submitted this assignment.")
 
        serializer.save(student=user.student)
+
+   
     def perform_update(self, serializer):
         user = self.request.user
         submission = self.get_object()
@@ -45,3 +65,9 @@ class AssignmentSubmissionViewSet(viewsets.ModelViewSet):
             serializer.save()
         else:
             raise PermissionDenied("You are not allowed to grade this submission.")
+    
+    @action(detail=True, methods=['get'], url_path='is-graded')
+    def is_graded(self, request, pk=None):
+        submission = self.get_object()
+        graded = submission.grade is not None
+        return Response({'is_graded': graded})
